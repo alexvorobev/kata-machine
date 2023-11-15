8804eeeab64700d35c9b440bd01b75fa35030330
Alexey Vorobyev <alexvorobev01@gmail.com>
2023-11-15 22:09:27 +0700

    bubble and quick sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,11 @@
 export default function bubble_sort(arr: number[]): void {
-
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = 0; j < arr.length - 1; j++) {
+      if(arr[i] < arr[j]) {
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
 }
--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,37 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], low: number, high: number) {
+  if(low >= high) {
+    return;
+  }
 
+  const pivotIndex = partition(arr, low, high);
+
+  qs(arr, low, pivotIndex - 1);
+  qs(arr, pivotIndex + 1, high);
+}
+
+function partition(arr: number[], low: number, high: number) {
+  const pivot = arr[high];
+
+  let idx = low - 1;
+
+  for (let i = low; i < high; i++) {
+    if(arr[i] <= pivot) {
+      idx++;
+
+      const temp = arr[i];
+      arr[i] = arr[idx];
+      arr[idx] = temp;
+    }
+  }
+
+  idx++;
+  arr[high] = arr[idx];
+  arr[idx] = pivot;
+
+  return idx;
+}
+
+
+export default function quick_sort(arr: number[]): void {
+  qs(arr, 0, arr.length - 1);
 }
